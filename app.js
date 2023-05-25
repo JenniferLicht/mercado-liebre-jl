b31035ba00e12a81eefe6eb2a57ada130afa67d8
jennifer lichtensztejn <jenniferlichtensztejn@gmail.com>
2023-05-24 22:58:16 -0300

    se modificó app.js

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ app.listen(process.env.PORT || 3000, () => {
 })
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html')); 
+    res.sendFile(path.resolve(__dirname, '/views/home.html')); 
 });
 
 app.get("/register", (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html')); 
+    res.sendFile(path.resolve(__dirname, '/views/register.html')); 
 });
 
 app.get("/login", (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html')); 
+    res.sendFile(path.resolve(__dirname, '/views/login.html')); 
 });
