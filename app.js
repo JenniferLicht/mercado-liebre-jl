c4f3cef4a29e385199c3ff0bc2b665e538254807
jennifer lichtensztejn <jenniferlichtensztejn@gmail.com>
2023-05-27 10:14:39 -0300

    se agregaron hovers´al login button y márgen al listado del nav

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ const path = require("path");
 
 const app = express();
 
-app.use( express.static("Public") );
+const publicPath = path.resolve(__dirname, './public'); 
+app.use( express.static(publicPath) );
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor corriendo en el puerto 3000");
 })
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '/Views/home.html')); 
+    res.sendFile(path.resolve(__dirname, './views/home.html')); 
 });
 
 app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname, '/Views/register.html')); 
+    res.sendFile(path.resolve(__dirname, './views/register.html')); 
 });
 
 app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, '/Views/login.html')); 
+    res.sendFile(path.resolve(__dirname, './views/login.html')); 
 });
